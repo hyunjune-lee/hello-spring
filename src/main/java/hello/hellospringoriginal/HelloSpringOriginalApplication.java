d20f15b14ae8520a7dce988d47202e787e6bcca5
Hyunjune Lee <44167177+hyunjune-lee@users.noreply.github.com>
2021-06-15 00:39:20 +0900

    Initial commit

---
+++ src/main/java/hello/hellospringoriginal/HelloSpringOriginalApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospringoriginal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringOriginalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringOriginalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/hellospringoriginal/HelloSpringOriginalApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospringoriginal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringOriginalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
